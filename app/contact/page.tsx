93bcf470310e5d16e4331c6b9a833617cca8a195
tricky <trickya23@gmail.com>
2024-05-11 12:27:49 +0000

    update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,21 +1,139 @@
-import ContactForm from "@/components/contactform";
-import ContactDetails from "@/components/contactdetails";
+import { MapPin, Mail, Phone } from 'lucide-react';
+import Link from "next/link"
 
-export default function Page() {
+export default function Contact() {
   return (
-    <main className="min-h-screen flex flex-col p-8 md:p-12">
-      <section>
-        <h1 className="capitalize font-bold text-4xl md:text-6xl p-8 md:p-14 text-center">contact us</h1>
-      </section>
-      <section className="flex flex-col justify-evenly min-h-screen md:grid grid-cols-2 md:gap-6 max-w-screen-2xl p-6 md:p-8">
-        <div className="pb-10 md:pb-0">
-          <ContactDetails />
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-base font-semibold text-indigo-600 uppercase tracking-wide">
+            Contact Us
+          </h2>
+          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+            Get in Touch
+          </p>
+          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+            Have a question or need assistance? Contact us, and our friendly team will be happy to help.
+          </p>
         </div>
-        {/* <hr className="block w-full mx-auto border-slate-500 md:hidden" /> */}
-        <div className="pt-10 md:pt-0">
-        <ContactForm />
+        <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="pt-6">
+            <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+              <div className="-mt-6">
+                <div>
+                  <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                    <MapPin className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                  Address
+                </h3>
+                <p className="mt-5 text-base text-gray-500">
+                  5th Avenue, Tema Community 11
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="pt-6">
+            <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+              <div className="-mt-6">
+                <div>
+                  <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                    <Mail className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                  Email
+                </h3>
+                <p className="mt-5 text-base text-gray-500">
+                  Goldenstonelogistics@gmail.com
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="pt-6 max-w-screen-lg">
+            <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+              <div className="-mt-6">
+                <div>
+                  <span className="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                    <Phone className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                  Phone
+                </h3>
+                <p className="mt-5 text-base text-gray-500">
+                  050 260 9222 <br />
+                  054 338 8670
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-    </main>
-  )
+
+        <div className="mt-12 md:mt-24 max-w-screen-md mx-auto">
+          <form className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <div className="mt-1">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Message
+              </label>
+              <div className="mt-1">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows={5}
+                  className="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <button
+                type="submit"
+                className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
